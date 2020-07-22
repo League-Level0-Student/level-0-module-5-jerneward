4fd85a681c0c31bc02bbd3b8fe96fcda69708c05
root <root@3dae7ebf12e9>
2020-07-22 23:55:04 +0000

    Finished snowflake, working on amazing rings.

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,6 +2,8 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class Snowflake {
 	
@@ -9,47 +11,57 @@ public class Snowflake {
 
 		// #1. Make a new Robot
 
+Robot Rob = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
+Rob.setX(300);
+Rob.setY(300);
 
 		// #3. Put the robot's pen down
-
+Rob.penDown();
 
 		// #4. Set the robot’s speed to 5
-
+Rob.setSpeed(5);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
+for (int i = 0; i < 6; i++) {
+	
 
 			
 			// #6. Set the pen color to black
 
-			
+			Rob.setPenColor(Color.BLACK);
 			// #7. move the robot by 50
-
+Rob.move(50);
 			
 			// #8. turn the robot to the right by 60
-			
+		Rob.turn(60);	
 			
 			// #12. set the pen color to blue for even numbers of i
-
+if(i%2 == 0) {
+	Rob.setPenColor(Color.blue);
+}
 			
 			// #13. set the pen color to red for all odd numbers of i
-			
+else {
+	Rob.setPenColor(Color.red);
+}
 			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
+			for (int j = 0; j < 6; j++) {
+				
 			
 			
 					// #10. turn the robot to the left by 60
-		
+	Rob.turn(-60);
 					
 					// #11. move the robot by 50 (play the start button)
-					
+			Rob.move(50);	
 				
-			
+			}
 			// #14. set the pen color to black
-				
+				Rob.setPenColor(Color.black);
 		
-	}
+	}}
 
 }
--- src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
+++ src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
@@ -1,16 +1,42 @@
 
 void setup() {
-  
-}
 
+  size(500, 450);
+}
+int x1 = 150;
+int x2 = 350;
 void draw() {
   
   // Go to the recipe to run the demonstration before starting this program
-  
-  /*
-  Use the code from your Bullseye program to draw the rings shown in this recipe.
-  You can use the noFill() command to make your ellipses transparent.
-  Make the rings move past each other.
-  When the rings hit the side of the sketch, make them reverse their direction.
-  */
+  background(#DEDEDE);
+  for (int i = 40; i > 0; i --) { 
+    if (i%2 == 0) {
+      fill(#DEDEDE);
+    } else {
+      fill(#030000);
+    }
+    ellipse(x1, 200, i*5, i*5);
+  }
+   for (int i = 40; i > 0; i --) { 
+    if (i%2 == 0) {
+      fill(#DEDEDE);
+    } else {
+      fill(#030000);
+    }
+    ellipse(x2, 200, i*5, i*5);
+  }
+  x1 ++ ;
+  if(x1 == 500){
+    x1 = 0;
+  }   
+  x2 --;
+  if(x2 == 0){
+    x2 = 500;
+  }
 }
+/*
+  Use the code from your Bullseye program to draw the rings shown in this recipe.
+ You can use the noFill() command to make your ellipses transparent.
+ Make the rings move past each other.
+ When the rings hit the side of the sketch, make them reverse their direction.
+ */
